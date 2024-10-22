cb91e8ae6f8ba764acf54bbb3a8f5cae0cd61f53
ediaz13 <diazemanuel27@gmail.com>
2024-10-22 17:59:34 -0300

    Persistance: Update Task Status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -24,29 +24,19 @@ export class TasksController {
   deleteTask(@Param('id') id: string): Promise<void> {
     return this.tasksService.deleteTaskById(id);
   }
-
-  /* @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
-    //if we have a filter defined, call tasksService.getTasksWithFilters
-    //otherwise, call tasksService.getAllTasks
-    if(Object.keys(filterDto).length) {
-      return this.tasksService.getTasksWithFilters(filterDto);
-    } else {
-      return this.tasksService.getAllTasks();
-    }
-
-    return this.tasksService.getAllTasks();
-  }
-
   
-
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
     @Body() updateTaskStatusDto : UpdateTaskStatusDto,
-  ): Task {
+  ): Promise<Task> {
     const { status } = updateTaskStatusDto;
     return this.tasksService.updateTaskStatus(id, status);
-  } */
+  }
+
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): Promise<Task[]> {
+   return this.tasksService.getTasks(filterDto);
+  }
 
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -38,37 +38,17 @@ export class TasksService {
         }
     }
 
-    /* 
-    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-        const { status, search } = filterDto;
-        let tasks = this.getAllTasks();
-
-        if(status) {
-            tasks = tasks.filter(task => task.status === status);
-        }
-
-        if(search) {
-            tasks = tasks.filter(task => 
-                task.title.includes(search) ||
-                task.description.includes(search)
-            );
-        }
-
-        return tasks;
-    }
     
 
-    getAllTasks(): Task[] {
-        return this.tasks;
+    async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
+        const task = await this.getTaskById(id);
+        task.status = status;
+
+        await this.taskRepository.save(task);
+        return task;
     }
 
-   
-
-   
-
-    updateTaskStatus(id: string, status: TaskStatus) {
-        const task = this.getTaskById(id);
-        task.status = status;
-        return task;
-    } */
+    getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+        return this.taskRepository.getTasks(filterDto);
+    }
 }
