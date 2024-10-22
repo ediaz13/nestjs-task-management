e537f0dcae05987c25fb5555457b67738266614d
ediaz13 <diazemanuel27@gmail.com>
2024-10-22 19:35:30 -0300

    Feature: Signing Up

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,14 @@
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-    // This is a custom method that we can use to find a user by username
+    // This is a custom method that we can use to create a user
+    async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentialsDto;
+
+        const user = this.create({ username, password });
+        await this.save(user);
+    }
 }   
