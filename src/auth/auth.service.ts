bb029c56982c8fb9189036f807e95f4b6650a7c6
ediaz13 <diazemanuel27@gmail.com>
2024-10-23 00:45:01 -0300

    Signing a JWT Token on Sign In (Authentication)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,36 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { InjectRepository } from "@nestjs/typeorm";
-import { UsersRepository } from "./users.repository";
-import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersRepository } from './users.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import * as bycrypt from 'bcrypt';
+import { JwtPayload } from './jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectRepository(UsersRepository)
-        private usersRepository: UsersRepository,
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
 
-    ) {}
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
 
-    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-        return this.usersRepository.createUser(authCredentialsDto);
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOne({ username });
+
+    if (user && (await bycrypt.compare(password, user.password))) {
+      const payload: JwtPayload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Invalid credentials');
     }
-
-    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
-        const { username, password } = authCredentialsDto;
-        const user = await this.usersRepository.findOne({ username });
-
-        if (user && (await bycrypt.compare(password, user.password))) {
-            return 'Success';
-        } else {
-            throw new UnauthorizedException('Invalid credentials');
-        }
-    }
+  }
 }
